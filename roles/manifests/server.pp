81a247c77e22fb9a66934e88517c49670b090b99
Tim Meusel <tim@bastelfreak.de>
2019-10-28 23:23:01 +0100

    add server role

---
+++ roles/manifests/server.pp
@@ -0,0 +1,21 @@
+class roles::server {
+  ensure_packages(['unzip'])
+  class{'consul':
+    version        => '1.6.1',
+    pretty_config  => true,
+    enable_beta_ui => true,
+    config_hash    => {
+      'bind_addr'            => $facts['networking']['ip'],
+      'bootstrap_expect'     => 1,
+      'data_dir'             => '/opt/consul',
+      'datacenter'           => 'NBG',
+      'log_level'            => 'INFO',
+      'node_name'            => $facts['fqdn'],
+      'server'               => true,
+      'disable_update_check' => true,
+      'enable_script_checks' => true,
+      'ui'                   => true,
+    },
+    require        => Package['unzip'],
+  }
+}
