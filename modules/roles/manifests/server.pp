12ce1a0622713f513be4a0399f28dd725dd508f2
Tim Meusel <tim@bastelfreak.de>
2019-10-30 23:21:58 +0100

    deploy epel

--- modules/roles/manifests/server.pp
+++ modules/roles/manifests/server.pp
@@ -1,4 +1,19 @@
 class roles::server {
+  # epel is needed by foreman and ferm
+  # foreman could provide epel for us, but we need to apply the basiscs class before :(
+  $osreleasemajor = $facts['os']['release']['major']
+  $epel_gpgkey = $osreleasemajor ? {
+    '7'     => 'https://fedoraproject.org/static/352C64E5.txt',
+    default => 'https://fedoraproject.org/static/0608B895.txt',
+  }
+  yumrepo { 'epel':
+    descr      => "Extra Packages for Enterprise Linux ${osreleasemajor} - \$basearch",
+    mirrorlist => "https://mirrors.fedoraproject.org/metalink?repo=epel-${osreleasemajor}&arch=\$basearch",
+    baseurl    => "http://download.fedoraproject.org/pub/epel/${osreleasemajor}/\$basearch",
+    enabled    => 1,
+    gpgcheck   => 1,
+    gpgkey     => $epel_gpgkey,
+  }
   ensure_packages(['unzip'])
   class{'consul':
     version        => '1.6.1',
@@ -18,7 +33,7 @@ class roles::server {
     },
     require        => Package['unzip'],
   }
-  include prometheus
+  include prometheus::server
   include nginx
   include ferm
   include ipset
