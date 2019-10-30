0f4902e42a6d2e18a6a8934b487c3118f7963d47
Tim Meusel <tim@bastelfreak.de>
2019-10-30 23:14:46 +0100

    import client role

---
+++ modules/roles/manifests/client.pp
@@ -0,0 +1,9 @@
+class roles::client {
+  include ferm
+  include ipset
+  include nginx
+  include consul
+  class{'prometheus::node_exporter':
+    extra_options => '--web.listen-address 127.0.0.1:9100',
+  }
+}
