b701646d042437ca3f151f9d706be77f0fdc6b87
Tim Meusel <tim@bastelfreak.de>
2019-11-01 16:48:32 +0100

    cleanup example

--- examples/node_exporter_consul.pp
+++ examples/node_exporter_consul.pp
@@ -1,5 +1,8 @@
 class profiles::node_exporter {
   # install consul
+  $ipfact = "networking.ip6"
+  $ipv6 = $facts['networking']['ip6']
+  $consulnodeips = query_nodes("Class[Roles::Consul] and $ipfact != '${ipv6}'", $ipfact)
   $config_hash = {
     'bind_addr'            => facts['networking']['ip6'],
     'data_dir'             => '/opt/consul',
@@ -9,30 +12,18 @@ class profiles::node_exporter {
     'server'               => false,
     'disable_update_check' => true,
     'encrypt'              => 'my_magic_key',
-    'verify_outgoing'      => true,
-    'verify_incoming'      => true,
-    'ca_file'              => '/etc/puppetlabs/puppet/ssl/certs/ca.pem',
-    'cert_file'            => "/etc/puppetlabs/puppet/ssl/certs/${trusted['certname']}.pem",
     'key_file'             => "/etc/consul.d/${trusted['certname']}.pem",
     'retry_join'           => map(sort($consulnodeips)) |$ip| { "[${ip}]" },
     'enable_script_checks' => true,
   }
   class { 'consul':
     pretty_config   => true,
-    #install_method => $install_method, # comes via hiera
     bin_dir         => '/usr/bin',
     config_dir      => '/etc/consul.d',
     extra_options   => '-raft-protocol 3',
     config_hash     => $config_hash,
     enable_beta_ui  => false,
     require         => Package['unzip'],
-  }
-  file { "/etc/consul.d/${trusted['certname']}.pem":
-    ensure => 'file',
-    owner  => 'consul',
-    group  => 'consul',
-    mode   => '0400',
-    source => "/etc/puppetlabs/puppet/ssl/private_keys/${trusted['certname']}.pem",
   }
 
   # register node exporter in consul
