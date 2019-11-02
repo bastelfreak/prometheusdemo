13ffaed5dded1d72ca7ab69597e770fcb49b92d0
Tim Meusel <tim@bastelfreak.de>
2019-11-02 17:31:00 +0100

    update exporters

--- modules/roles/manifests/client.pp
+++ modules/roles/manifests/client.pp
@@ -52,6 +52,10 @@ class roles::client {
   }
 
   class{'prometheus::node_exporter':
+    collectors_enable => ['diskstats','filesystem','loadavg','meminfo','netdev','netstat','stat','time',
+                          'interrupts','tcpstat', 'textfile', 'systemd', 'qdisc', 'processes',
+                          'mountstats', 'logind', 'loadavg', 'entropy', 'edac',
+                          'cpufreq', 'cpu', 'conntrack', 'arp'],
     extra_options => '--web.listen-address 127.0.0.1:9100',
     version       => '0.18.1',
   }
--- modules/roles/manifests/server.pp
+++ modules/roles/manifests/server.pp
@@ -151,6 +151,10 @@ class roles::server {
   }
 
   class{'prometheus::node_exporter':
+    collectors_enable => ['diskstats','filesystem','loadavg','meminfo','netdev','netstat','stat','time',
+                          'interrupts','tcpstat', 'textfile', 'systemd', 'qdisc', 'processes',
+                          'mountstats', 'logind', 'loadavg', 'entropy', 'edac',
+                          'cpufreq', 'cpu', 'conntrack', 'arp'],
     extra_options => '--web.listen-address 127.0.0.1:9100',
     version       => '0.18.1',
   }
