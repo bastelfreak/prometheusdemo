5b5425805cabcc6bc9266612d00833b1b1d99d33
Tim Meusel <tim@bastelfreak.de>
2019-11-02 16:28:21 +0100

    consul: fix dependencies

--- modules/roles/manifests/server.pp
+++ modules/roles/manifests/server.pp
@@ -70,12 +70,13 @@ class roles::server {
     require        => Package['unzip'],
   }
   file { "/etc/consul.d/${trusted['certname']}.pem":
-    ensure         => 'file',
-    owner          => 'consul',
-    group          => 'consul',
-    mode           => '0400',
-    source         => "/etc/puppetlabs/puppet/ssl/private_keys/${trusted['certname']}.pem",
-    notify         => Class['consul::reload_service'],
+    ensure  => 'file',
+    owner   => 'consul',
+    group   => 'consul',
+    mode    => '0400',
+    source  => "/etc/puppetlabs/puppet/ssl/private_keys/${trusted['certname']}.pem",
+    notify  => Class['consul::reload_service'],
+    require => Class['consul::config'],
   }
 
   class{'prometheus::server':
